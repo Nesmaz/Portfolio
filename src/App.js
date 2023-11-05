206f7a6b2cb3bbc0790861ab801ef9615e0bbd9c
Nesmaz <nesmabdelazim@gmail.com>
2023-11-05 13:18:39 +0200

    .

--- src/App.js
+++ src/App.js
@@ -5,8 +5,26 @@ import About from './components/About/About';
 import Work from './components/Work/Work';
 import Contact from './components/Contact/Contact';
 import { FaArrowCircleUp } from 'react-icons/fa';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    window.addEventListener('scroll', scrrFunction);
+
+    return () => {
+      window.removeEventListener('scroll', scrrFunction);
+    };
+  }, []);
+
+   function scrrFunction(){
+    const scrr = document.getElementById('scrr');
+    if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600 ) {
+      scrr.style.display = "block";
+    } else {
+      scrr.style.display = "none";
+    }
+  }
 
   return (
     <div className="App">
